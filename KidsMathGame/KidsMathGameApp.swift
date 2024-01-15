bc9e20e56d4d9c97454716f186e51848c5635dcf
arshad <ak849496@gmail.com>
2024-01-15 17:45:40 +0500

    Git file updated

--- KidsMathGame/KidsMathGameApp.swift
+++ KidsMathGame/KidsMathGameApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct KidsMathGameApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
         }
     }
 }
---
+++ KidsMathGame/Match/MatchView.swift
@@ -0,0 +1,18 @@
+//
+//  MatchView.swift
+//  KidsMathGame
+//
+//  Created by Arshad Mustafa on 15/01/2024.
+//
+
+import SwiftUI
+
+struct MatchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MatchView()
+}
