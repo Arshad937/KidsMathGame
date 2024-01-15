3e68fa173e585dac10b7d3915b58b7dc2e5c9ea7
arshad <ak849496@gmail.com>
2024-01-15 10:55:27 +0500

    Initial Commit

---
+++ KidsMathGame/KidsMathGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  KidsMathGameApp.swift
+//  KidsMathGame
+//
+//  Created by Arshad Mustafa on 15/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct KidsMathGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
